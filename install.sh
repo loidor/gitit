5f9a3dcd870026932047b714fbff564a2cc8ea5e
loidor <erik.ohrn@gmail.com>
2020-05-12 16:55:27 +0200

    Best commit ever

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 cp gitit /usr/local/bin/gitit
+
+echo "Choose your language: "
+select language in "English" "Swedish"; do
+
+  case $language in
+    English )
+      echo 'lang=eng' > /home/$SUDO_USER/.config/gitit.conf
+      exit
+      ;;
+    Swedish )
+      echo 'lang=eng' > /home/$SUDO_USER/.config/gitit.conf
+      exit
+      ;;
+  esac
+done
